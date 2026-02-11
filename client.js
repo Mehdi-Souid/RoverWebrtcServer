3cb8d1eb3be847ae93708365d8bb7877cbdd64f3
Mehdi <mehdisouid0@gmail.com>
2026-02-11 19:43:06 +0100

    Update to paid Metered.ca TURN servers for reliable connectivity

--- client.js
+++ client.js
@@ -2,46 +2,26 @@ let ws = null;
 let peerConnection = null;
 
 const ICE_SERVERS = [
-    {urls: 'stun:stun.l.google.com:19302'},
-    {urls: 'stun:stun1.l.google.com:19302'},
-    {urls: 'stun:stun2.l.google.com:19302'},
-    {urls: 'stun:stun3.l.google.com:19302'},
-    {urls: 'stun:stun4.l.google.com:19302'},
+    {urls: 'stun:stun.relay.metered.ca:80'},
     {
-        urls: 'turn:openrelay.metered.ca:80',
-        username: 'openrelayproject',
-        credential: 'openrelayproject'
+        urls: 'turn:standard.relay.metered.ca:80',
+        username: '919ce4bb2a1d5222c80d0cd0',
+        credential: 'JfmmY9P3NZQVvm71'
     },
     {
-        urls: 'turn:openrelay.metered.ca:443',
-        username: 'openrelayproject',
-        credential: 'openrelayproject'
+        urls: 'turn:standard.relay.metered.ca:80?transport=tcp',
+        username: '919ce4bb2a1d5222c80d0cd0',
+        credential: 'JfmmY9P3NZQVvm71'
     },
     {
-        urls: 'turn:openrelay.metered.ca:443?transport=tcp',
-        username: 'openrelayproject',
-        credential: 'openrelayproject'
-    },
-    {urls: 'stun:global.stun.twilio.com:3478'},
-    {
-        urls: 'turn:a.relay.metered.ca:80',
-        username: 'openrelayproject',
-        credential: 'openrelayproject'
+        urls: 'turn:standard.relay.metered.ca:443',
+        username: '919ce4bb2a1d5222c80d0cd0',
+        credential: 'JfmmY9P3NZQVvm71'
     },
     {
-        urls: 'turn:a.relay.metered.ca:80?transport=tcp',
-        username: 'openrelayproject',
-        credential: 'openrelayproject'
-    },
-    {
-        urls: 'turn:a.relay.metered.ca:443',
-        username: 'openrelayproject',
-        credential: 'openrelayproject'
-    },
-    {
-        urls: 'turn:a.relay.metered.ca:443?transport=tcp',
-        username: 'openrelayproject',
-        credential: 'openrelayproject'
+        urls: 'turns:standard.relay.metered.ca:443?transport=tcp',
+        username: '919ce4bb2a1d5222c80d0cd0',
+        credential: 'JfmmY9P3NZQVvm71'
     }
 ];
 
